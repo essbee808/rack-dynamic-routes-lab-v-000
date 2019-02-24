9784443133951fd966c21af97a90ae844c167be5
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-24 03:41:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
   
     item_name = req.path.split("/items/").last
     
-    if req.path=="/items/" + item_name
+    if req.path=="/items/" + item_name && @@items.include?(item_name)
         @@items.each do |el| 
           if el.name == item_name
             resp.write el.price
