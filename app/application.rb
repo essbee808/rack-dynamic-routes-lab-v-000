7244bada1e958de9ec3eae586f740cbdb933fe2c
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-24 03:56:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,9 +6,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
   
-    item_name = req.path.split("/items/").last
-    
     if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
       if @@items.include?(item_name)
        item =  @@items.find{|el| el.name == item_name}
         resp.write "#{item.price}"
