827dc56c98adc123c395b9924bddd2eb2c2bcf84
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-24 03:12:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
+    
+    
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if @@items.include?(item_name)
