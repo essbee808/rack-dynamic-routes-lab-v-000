82ee2e2a3bc5a24f13ae5204832b02003a5f8672
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-24 03:19:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     if req.path.match(/items/) && @@items.include?(item_name)
         item = @@items.select{|el| el.name == item_name}
         resp.status = 200
-    elsif 
+    elsif req.path.match(/items/) && !@@items.include?(item_name)
         resp.write "Item not found"
         resp.status = 400
       end
