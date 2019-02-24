d8c120ed4ba453c8542b2ec66007c48e9330e43c
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-24 04:32:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
   
     if req.path=="/items/#{item_name}"
       item = @@items.find{|i| i.name == item_name}
-        if 
+        if @@items.include?(item)
       
         # if @@items.include?(item_name)
         #   item =  @@items.find{|el| el.name == item_name}
