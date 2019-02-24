14ac75a530b353f98ad017dc1c2baf2f7d25f80c
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-24 03:25:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,7 +13,7 @@ class Application
         item = @@items.select{|el| el.name == item_name}
         resp.write item.price
         resp.status = 200
-    elsif req.path.match(/items/)
+      else req.path.match(/items/)
         resp.write "Item not found"
         resp.status = 400
     
