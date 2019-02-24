4ad9028c04a23e3a46e503aa9bfd8d15666c547b
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-24 03:57:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,8 +10,8 @@ class Application
       item_name = req.path.split("/items/").last
         if @@items.include?(item_name)
           item =  @@items.find{|el| el.name == item_name}
-          resp.write "#{item.price}"
-          resp.status = 200
+          resp.write "'#{item.price}'"
+          resp.status = 200'
         else 
         resp.write "Item not found"
         resp.status = 400
